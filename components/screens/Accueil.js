700815bb2820aa860aecaed8f37692642d68f771
Julian Labatut <julian.labatut@hotmail.com>
2020-11-04 16:22:12 +0100

    update accueil, passage de classe à fonction

--- components/screens/Accueil.js
+++ components/screens/Accueil.js
@@ -1,5 +1,6 @@
 // Imports Modules
-import React from 'react';
+import React, {useState, useContext, useEffect} from 'react';
+import {Context} from '../utils/Store';
 import {
   View,
   StyleSheet,
@@ -19,75 +20,83 @@ import Horloge from '../screens/Horloge';
 
 var avatar = require('../../assets/avatar.png');
 
-export default class Accueil extends React.Component {
-  state = {
-    name: 'Gaston',
-    kcal: '5400',
-    km: '234.0',
-    watts: '40000',
-    avatar: avatar,
-  };
-  render() {
-    return (
-      <SafeAreaView style={styles.container}>
-        <Image style={styles.fond} source={require('../../assets/fond.png')} />
-          <View style={[styles.header, {width: '100%'}]}>
-            <View style={styles.item}></View>
-            <View style={styles.item}>
-              <LogoMin />
-            </View>
-            <View style={styles.item}>
-              <Battery />
-              <Horloge />
-            </View>
-          </View>
-          <View style={[styles.middle, {width: '100%'}]}>
-            <View style={styles.midTop}>
-              <Text style={styles.nom}>
-                {this.state.name}
-              </Text>
-            </View>
-            <View style={styles.midMid}>
-              <View style={[styles.midItem]}>
-                <Text
-                  style={styles.chiffres}>
-                  {this.state.kcal}
-                </Text>
-                <Text style={[styles.midText]}>kcal <Text style={{color: '#5FCDFA'}}>cumulées</Text></Text>
-              </View>
-              <View style={styles.midItem}>
-                <TouchableHighlight
-                  onPress={() => this.props.navigation.navigate('Classements')}>
-                  <Image source={this.state.avatar} />
-                </TouchableHighlight>
-              </View>
-              <View style={[styles.midItem]}>
-                <Text
-                  style={styles.chiffres}>
-                  {this.state.km}
-                </Text>
-                <Text style={[styles.midText]}>km <Text style={{color: '#5FCDFA'}}>cumulés</Text></Text>
-              </View>
-            </View>
-            <View style={styles.midBot}>
-              <Text style={styles.chiffres}>
-                {this.state.watts}
-              </Text>
-              <Text style={[styles.midText]}>wh <Text style={{color: '#5FCDFA'}}>produits</Text></Text>
-            </View>
-          </View>
-          <View style={styles.footer}>
-            <Cercle />
-            <Text
-              style={styles.go}
-              onPress={() => this.props.navigation.navigate('ListeDefis')}>
-              GO
+export default function Accueil(props) {
+  // state = {
+  //   name: 'Gaston',
+  //   kcal: '5400',
+  //   km: '234.0',
+  //   watts: '40000',
+  //   avatar: avatar,
+  // };
+  const [state, setState] = useContext(Context);
+
+  useEffect(() => {
+    props.navigation.addListener('beforeRemove', e => {
+      // e.preventDefault();
+      // props.navigation.popToTop();
+      // props.navigation.navigate('Demarrage');
+      return;
+    });
+  });
+
+  return (
+    <SafeAreaView style={styles.container}>
+      <Image style={styles.fond} source={require('../../assets/fond.png')} />
+      <View style={[styles.header, {width: '100%'}]}>
+        <View style={styles.item} />
+        <View style={styles.item}>
+          <LogoMin />
+        </View>
+        <View style={styles.item}>
+          <Battery />
+          <Horloge />
+        </View>
+      </View>
+      <View style={[styles.middle, {width: '100%'}]}>
+        <View style={styles.midTop}>
+          <Text style={styles.nom}>{state.user.username}</Text>
+        </View>
+        <View style={styles.midMid}>
+          <View style={[styles.midItem]}>
+            <Text style={styles.chiffres}>{state.kcal}</Text>
+            <Text style={[styles.midText]}>
+              kcal <Text style={{color: '#5FCDFA'}}>cumulées</Text>
             </Text>
           </View>
-          <NavApp style={{flex:1, justifyContent:'center'}}navigation={this.props.navigation} />
-      </SafeAreaView>
-    );
-  }
+          <View style={styles.midItem}>
+            <TouchableHighlight
+              onPress={() => props.navigation.navigate('Classements')}>
+              <Image source={avatar} />
+            </TouchableHighlight>
+          </View>
+          <View style={[styles.midItem]}>
+            <Text style={styles.chiffres}>{state.km}</Text>
+            <Text style={[styles.midText]}>
+              km <Text style={{color: '#5FCDFA'}}>cumulés</Text>
+            </Text>
+          </View>
+        </View>
+        <View style={styles.midBot}>
+          <Text style={styles.chiffres}>{state.watts}</Text>
+          <Text style={[styles.midText]}>
+            wh <Text style={{color: '#5FCDFA'}}>produits</Text>
+          </Text>
+        </View>
+      </View>
+      <View style={styles.footer}>
+        <Cercle />
+        <Text
+          style={styles.go}
+          onPress={() => props.navigation.navigate('ListeDefis')}>
+          GO
+        </Text>
+      </View>
+      <NavApp
+        style={{flex: 1, justifyContent: 'center'}}
+        navigation={props.navigation}
+      />
+    </SafeAreaView>
+  );
 }
 
 const styles = StyleSheet.create({
@@ -141,22 +150,22 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     zIndex: 100,
   },
-nom:{
-    color:"white",
-  fontSize:50,
-  fontFamily:'GnuolaneRG-Regular'
-},
+  nom: {
+    color: 'white',
+    fontSize: 50,
+    fontFamily: 'GnuolaneRG-Regular',
+  },
   midText: {
     color: 'white',
     fontSize: 30,
     textTransform: 'uppercase',
-    fontFamily:'TallFilms'
+    fontFamily: 'TallFilms',
   },
-  chiffres:{
+  chiffres: {
     color: 'white',
     fontSize: 30,
     textTransform: 'uppercase',
-    fontFamily:'GnuolaneRG-Regular'
+    fontFamily: 'GnuolaneRG-Regular',
   },
 
   fond: {
@@ -171,7 +180,7 @@ nom:{
   footer: {
     flex: 1,
     flexDirection: 'row',
-    alignItems:'flex-end',
+    alignItems: 'flex-end',
     justifyContent: 'center',
     zIndex: 100,
     bottom: '20%',
