782bb0910dacf343c9602a4aecea81da4be6f7cf
Romain V <romain.vilayleck@alf-environnement.com>
2020-12-21 11:10:21 +0100

    remove react-native canvas

--- components/screens/afficheurConpteur.js
+++ components/screens/afficheurConpteur.js
@@ -11,7 +11,6 @@ import {
     Button,
     Dimensions
 } from 'react-native'
-//import Canvas, {Image as CanvasImage, Path2D, ImageData} from 'react-native-canvas';
 import Svg, {  Path, G } from 'react-native-svg'
 //assets
 import Fleche from "../../assets/fleche";
@@ -44,36 +43,7 @@ export default class AfficheurCompteur extends React.Component {
         }
     }
 
-/*
-    async handleCompteur() {
-        const context = this.refs.canvas.getContext('2d');
-        canvas.width = Dimensions.get('window').width;
-        canvas.height = 350;
-        const {angle} = this.props
-        const fond = new CanvasImage(canvas);
-        fond.src = fondUri
-        const fondb = new CanvasImage(canvas);
-        fondb.src = fondBulleUri
-        const aiguille = new CanvasImage(canvas)
-        aiguille.src = aiguilleUri
-        fond.addEventListener('load', e => {
-            context.drawImage(fond, canvas.width/64, canvas.height/64, 400, 400)
-            /*context.drawImage(fondb, 0,0, 20,20) message out of range
-            context.beginPath()
-            context.strokeStyle = '#5FCDFA';
-            context.arc(220, 200, 100, 2 * Math.PI / 3, 3 * Math.PI / 4, false);
-            context.lineWidth = 10;
-            context.stroke();
-            context.save();
-            context.translate(canvas.width/2, canvas.width*0.55)
-            context.rotate((Math.PI / 180) * angle)
-            //context.translate(-canvas.width/2, -canvas.width/2)
-            context.drawImage(aiguille, -canvas.width/4, -canvas.width/4, 240, 240)
-            context.restore();
-            requestAnimationFrame(() => this.handleCompteur(this.canvas))
-        })
-    }
-    */
+
     animation(){
         let svg = ""
         for (let x = 0; x<this.props.i; x++){
@@ -93,7 +63,6 @@ export default class AfficheurCompteur extends React.Component {
     }
 
     render(){
-        //<Canvas ref="canvas"/>
         //M120,280 a125,125 0 1,1 110,10 cercle complet
         //`M120,280 a122,122 0 1,1 180,-100 ` cercle - 3/4
         //M120 280 C 92 279 66 244 72 221
